922f811c94a4d3486372215cdeb091db7814ffe3
Keenan Du Plessis <keenan.dup137@gmail.com>
2026-04-21 19:30:07 +0200

    Update site URL and sitemap links in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,13 @@ import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://silly-concha-ddcd81.netlify.app/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
+        "https://silly-concha-ddcd81.netlify.app/sitemap-index.xml",
+        "https://silly-concha-ddcd81.netlify.app/sitemap-0.xml",
       ],
     }),
     solidJs(),
